03f0783a971404a4f6398c5d26ef9d1d5c6a865e
victhangnguyen <97330371+victhangnguyen@users.noreply.github.com>
2023-12-29 06:25:15 +0700

    create route for webhooks user

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,7 +1,10 @@
 'use server'
 
+import Question from '@/database/question.model'
 import User from '@/database/user.model'
+import { revalidatePath } from 'next/cache'
 import { connectToDatabase } from '../mongoose'
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from './shared.types'
 
 export async function getUserById(params: any) {
   try {
@@ -15,5 +18,61 @@ export async function getUserById(params: any) {
   } catch (error) {
     console.log('Error: ', error)
     throw error
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase()
+
+    const newUser = await User.create(userData)
+
+    return newUser
+  } catch (error) {
+    console.log('Error: ', error)
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase()
+
+    const { clerkId, updateData, path } = params
+
+    await User.findOneAndUpdate({ clerkId }, updateData, { new: true })
+    //! new: true, So we want to create a new instance of a user in a database.
+
+    revalidatePath(path)
+  } catch (error) {
+    console.log('Error: ', error)
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase()
+
+    const { clerkId } = params
+
+    const user = await User.findOneAndDelete({ clerkId })
+
+    if (!user) {
+      throw new Error('User not found')
+    }
+
+    //! Delete the user from the database
+    //! and questions, answers, comments, etc.
+
+    //! get user question Ids
+    const userQuestionIds = await Question.find({ author: user._id }).distinct('_id')
+    console.log('userQuestionIds: ', userQuestionIds)
+
+    // TODO: delete user answers, comments, etc...
+
+    const deletedUser = await User.findByIdAndDelete(user._id)
+    
+    return deletedUser
+  } catch (error) {
+    console.log('Error: ', error)
   }
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
+// @type {import('next').NextConfig}
+
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: true, //! v13
     mdxRs: true,
     serverComponentsExternalPackages: ['mongoose']
   }
